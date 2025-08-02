3729f2e51e34bf16d8a808b8b60a385e6eb23763
AyyanNadeem01 <ayyan137a@gmail.com>
2025-08-03 01:51:58 +0500

    otp service for email and phone number built

---
+++ backend/services/twilioService.js
@@ -0,0 +1,46 @@
+const twilio =require("twilio");
+
+const accountSid=process.env.TWILIO_ACCOUNT_SID;
+const authToken=process.env.TWILIO_AUTH_TOKEN;
+const serviceSid=process.env.TWILIO_SERVICE_SID;
+
+const client = twilio (accountSid,authToken);
+
+const sendOtpToPhoneNumber =async(phoneNumber)=>{
+    try{
+        console.log("Sending OTP to this number",phoneNumber);
+        if(!phoneNumber){
+            throw new Error("phone number is required");
+        }
+        const response = await client.verify.v2.services(serviceSid).verifications.create({
+            to:phoneNumber,
+            channel:"sms"
+        });
+        console.log("this is my otp response",response);
+        return response;
+    }catch(error){
+        console.error(error);
+        throw new Error("Failed to send otp")
+    }
+}
+
+const verifyOtp =async(phoneNumber,otp)=>{
+    try{
+        console.log("this is my otp",otp);
+        console.log("this number",phoneNumber);
+        const response = await client.verify.v2.services(serviceSid).verificationChecks.create({
+            to:phoneNumber,
+            code:otp
+        });
+        console.log("this is my otp response",response);
+        return response;
+    }catch(error){
+        console.error(error);
+        throw new Error("otp verification failed")
+    }
+}
+
+module.exports={
+    sendOtpToPhoneNumber,
+    verifyOtp
+}
