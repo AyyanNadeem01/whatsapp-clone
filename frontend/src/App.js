12a9bb69cc233a1b48075a4c59ea1b518afa1050
AyyanNadeem01 <ayyan137a@gmail.com>
2025-08-09 15:15:36 +0500

    front end setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Login from './pages/user-login/Login';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/user-login" element={<Login/>}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
